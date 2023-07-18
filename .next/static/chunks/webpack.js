eb083112ca9a0e7d558a61a0ddf1ef87bfcfff66
agungd3v <agungd3v@gmail.com>
2023-07-18 08:36:39 +0700

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b701e9c02c79e9e"; }
+/******/ 		__webpack_require__.h = function() { return "b4bdb89db110a57c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ContentThree.tsx
+++ components/ContentThree.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { FaCaretDown } from "react-icons/fa6";
 
 export default function ContentThree() {
   const container = {
@@ -25,7 +26,7 @@ export default function ContentThree() {
                 className="w-full h-full absolute bg-white"
                 variants={{
                   hidden: {
-                    right: "-9%"
+                    right: "-99%"
                   },
                   visible: {
                     right: 0
@@ -52,7 +53,7 @@ export default function ContentThree() {
                 className="w-full h-full absolute bg-white"
                 variants={{
                   hidden: {
-                    left: "-9%"
+                    left: "-99%"
                   },
                   visible: {
                     left: 0
@@ -66,6 +67,46 @@ export default function ContentThree() {
           );
         })]}
       </motion.div>
+    </div>
+    <div className="text-black w-full h-full absolute overflow-hidden p-16" style={{zIndex: 4}}>
+      <div className="w-full h-full relative">
+        <div className="absolute s-wrap w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Requirement gathering and analysis</span>
+          <div className="sl-right">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+        <div className="absolute s-wrap-2 w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Design</span>
+          <div className="sl-right">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+        <div className="absolute s-wrap-3 w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Implementasi</span>
+          <div className="sl-right">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+        <div className="absolute s-wrap-4 w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Integration & testing</span>
+          <div className="sl-right">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+        <div className="absolute s-wrap-5 w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Verification</span>
+          <div className="sl-right">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+        <div className="absolute s-wrap-6 w-32 text-center h-32 rounded-full leading-4 border-primary flex items-center justify-center px-2">
+          <span>Operation & maintenance</span>
+          <div className="sl-right-last">
+            <FaCaretDown className="absolute" style={{right: "-10.5px", top: "7%"}} size={24} />
+          </div>
+        </div>
+      </div>
     </div>
   </>);
 }
